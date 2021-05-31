f732579e17052d9b8fc8d31af51e45865c4f204d
Renju Jose <renju.jose@testsigma.com>
2021-05-31 19:17:37 +0530

    [Boot]Change default logging to INFO

--- src/main/java/com/linkedin/learning/H2BootStrapData.java
+++ src/main/java/com/linkedin/learning/H2BootStrapData.java
@@ -4,13 +4,14 @@ import com.linkedin.learning.entity.ReservationEntity;
 import com.linkedin.learning.entity.RoomEntity;
 import com.linkedin.learning.repository.ReservationRepository;
 import com.linkedin.learning.repository.RoomRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 
-@Component
+@Component @Slf4j
 public class H2BootStrapData implements CommandLineRunner {
     @Autowired
     RoomRepository roomRepository;
@@ -19,25 +20,25 @@ public class H2BootStrapData implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Bootstrapping data in H2...");
+        log.info("Bootstrapping data in H2...");
         for(int i=0;i<3;i++) roomRepository.save(new RoomEntity(101+i,String.valueOf(200+(i*2))));
-        reservationRepository.save(
-                new ReservationEntity(LocalDate.parse("2021-02-02"), LocalDate.parse("2021-02-22"),getRoomById(2)));
-        reservationRepository.save(
-                new ReservationEntity(LocalDate.parse("2021-02-04"), LocalDate.parse("2021-02-21"),getRoomById(1)));
-        reservationRepository.save(
-                new ReservationEntity(LocalDate.parse("2021-05-24"), LocalDate.parse("2021-05-28"),getRoomById(1)));
-        System.out.println("H2 bootstrapped with "+roomRepository.count()+" rooms and "+
-                reservationRepository.count()+"reservations.");
+        reservationRepository.save(new ReservationEntity(
+                LocalDate.parse("2021-02-02"), LocalDate.parse("2021-02-22"),getRoomById(2)));
+        reservationRepository.save(new ReservationEntity(
+                LocalDate.parse("2021-02-04"), LocalDate.parse("2021-02-21"),getRoomById(1)));
+        reservationRepository.save(new ReservationEntity(
+                LocalDate.parse("2021-05-24"), LocalDate.parse("2021-05-28"),getRoomById(1)));
+        log.debug("H2 bootstrapped with {} rooms and {} reservations.",roomRepository.count(),reservationRepository.count());
+
+        //logging rooms
         Iterable<RoomEntity> roomEntities = roomRepository.findAll();
-        for (RoomEntity room: roomEntities) System.out.println(room.getId()+"-Room Number: "+room.getRoomNumber());
+        roomEntities.forEach( r -> log.debug("{} -Room Number: {}, Price {}",r.getId(),r.getRoomNumber(),r.getPrice()));
+
+        //logging reservations
         Iterable<ReservationEntity> reservationEntities = reservationRepository.findAll();
-        for (ReservationEntity reservationEntity: reservationEntities)
-            System.out.println(+reservationEntity.getId()+
-                "-Checkin: "+reservationEntity.getCheckin()+
-                ", Checkout: "+reservationEntity.getCheckout()+
-                ", Room: "+reservationEntity.getRoomEntity());
-    }
+        reservationEntities.forEach( r -> log.debug("{} -Checkin: {} , Checkout: {} , Room: {}",r.getId(),
+                r.getCheckin(),r.getCheckout(),r.getRoomEntity()));
+        }
 
     public RoomEntity getRoomById(int id){
         return roomRepository.findById(id).orElse(new RoomEntity(000,"0"));
