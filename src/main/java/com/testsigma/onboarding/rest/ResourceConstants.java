43247578bd0cad920e1abe35ac6afced9c0e0766
Renju Jose <renjujv@gmail.com>
2021-06-01 15:06:54 +0530

    [Boot] Spring security config
    - Added Spring security config classes
    - Added Thymeleaf templates

---
+++ src/main/java/com/testsigma/onboarding/rest/ResourceConstants.java
@@ -0,0 +1,11 @@
+package com.testsigma.onboarding.rest;
+
+public class ResourceConstants {
+    public static final String ROOM_RESERVATION_V1 = "/room/reservation/v1";
+    public static final String ROOM_RESERVATION_ALL = ROOM_RESERVATION_V1+"/all";
+    public static final String ROOM_RESERVATION_DELETE = ROOM_RESERVATION_V1+"/delete";
+    public static final String ROOM_RESERVATION_UPDATE = ROOM_RESERVATION_V1+"/update";
+    public static final String LOGIN = "/login";
+    public static final String HOME = "/home";
+    public static final String HELLO = "/hello";
+}
