66657d05058cc188baf6f7f3aa34a75752bd1ee2
Renju Jose <renjujv@gmail.com>
2021-06-03 20:09:47 +0530

    [Boot]Documentation
    - Added notnull validation for dates
    - Added documentation for rest

--- src/main/java/com/testsigma/onboarding/entity/Reservation.java
+++ src/main/java/com/testsigma/onboarding/entity/Reservation.java
@@ -1,36 +1,32 @@
 package com.testsigma.onboarding.entity;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.testsigma.onboarding.serialization.ReservationSerializer;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
-import java.util.List;
 
 @Entity
 @Table(name = "RESERVATION")
 @NoArgsConstructor
+@JsonSerialize(using = ReservationSerializer.class)
 public class Reservation {
 
     @Id @Getter @Setter @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Getter @Setter
+    @Getter @Setter @NotNull
     private LocalDate checkin;
 
-    @Getter @Setter
+    @Getter @Setter @NotNull
     private LocalDate checkout;
 
     @Getter @Setter @ManyToOne(targetEntity = Room.class)
     private Room bookedRoom;
 
-    public Reservation(LocalDate checkin, LocalDate checkout) {
-        this.checkin = checkin;
-        this.checkout = checkout;
-    }
-
-    public Reservation(LocalDate checkin, LocalDate checkout, Room bookedRoom) {
+    public Reservation(@NotNull LocalDate checkin, @NotNull LocalDate checkout, Room bookedRoom) {
         this.checkin = checkin;
         this.checkout = checkout;
         this.bookedRoom = bookedRoom;
--- src/main/java/com/testsigma/onboarding/rest/ReservationResource.java
+++ src/main/java/com/testsigma/onboarding/rest/ReservationResource.java
@@ -40,6 +40,14 @@ public class ReservationResource {
     ConversionService conversionService;
 
     //@DateTimeFormat(iso = DateTimeFormat.ISO.DATE,pattern = "dd-MM-yyyy") LocalDate checkin
+    /**
+     * Get the list of reservations for the provided checkin and checkout dates.
+     *
+     * @param checkin
+     * Checkin date for reservation
+     * @param checkout
+     * Checkout date for reservation
+     */
     @GetMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
     public Page<ReservableRoomResponse> getAvailableRooms(
             @RequestParam(value = "checkin") String checkin,
@@ -63,12 +71,20 @@ public class ReservationResource {
         return reservableRoomResponses;
     }
 
+    /**
+     * Get the list of all reservations.
+     */
     @GetMapping(path = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Room>> getAllAvailableRooms(){
         List<Room> room = roomRepository.findAll();
         return new ResponseEntity<List<Room>>(room,HttpStatus.OK);
     }
 
+    /**
+     * Get the details of specified room.
+     * @param roomId
+     * Id of the room
+     */
     @GetMapping(path = "/{roomId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Room> getRoomById(
             @PathVariable Integer roomId) {
@@ -76,6 +92,14 @@ public class ReservationResource {
         return new ResponseEntity<Room>(room,HttpStatus.OK);
     }
 
+    /**
+     * Make a reservation
+     * @param reservationRequest
+     * Reservation request containing
+     * roomID(Room to make reservation on)
+     * , checkin Date(Checkin date to create reservation)
+     * , checkoutDate(Checkout date to create reservation)
+     */
     //TODO Avoid reservation creation with unavailable room ID by checking roomEntity presence in roomRepository
     @PostMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE,
             consumes = MediaType.APPLICATION_JSON_VALUE)
@@ -95,6 +119,14 @@ public class ReservationResource {
         return new ResponseEntity<>(reservationResponse,HttpStatus.CREATED);
     }
 
+    /**
+     * Update a reservation
+     * @param reservationRequest
+     * Reservation update request containing
+     * roomID(Room to make reservation on)
+     * , checkin Date(Checkin date to create reservation)
+     * , checkoutDate(Checkout date to create reservation)
+     */
     @PutMapping(path = "/update/{reservationId}", produces = MediaType.APPLICATION_JSON_VALUE,
             consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ReservationResponse> updateReservation(
@@ -120,6 +152,11 @@ public class ReservationResource {
         return new ResponseEntity<>(reservationResponse,HttpStatus.ACCEPTED);
     }
 
+    /**
+     * Delete the reservation
+     * @param reservationId
+     * ID of reservation to be deleted
+     */
     //TODO Fix delete reservation by deleting dependent entity from other table
     @DeleteMapping(path = "/delete/{reservationId}")
     public ResponseEntity<String> deleteReservation(
